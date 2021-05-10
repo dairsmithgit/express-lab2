f8c3ce3b507501c4c77c009779a8baead3fd790a
Dair Smith <dair.code@gmail.com>
2021-05-10 13:27:16 -0400

    home page working

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import express from 'express';
+import home from './routes/home';
 
 const app = express();
 
@@ -7,6 +8,8 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", home);
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port: ${port}.`));
