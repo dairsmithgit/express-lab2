5894c605b384033856aede361bd8a0b033ae5775
Dair Smith <dair.code@gmail.com>
2021-05-10 12:03:40 -0400

    added stylesheet and setup port

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
 import path from 'path';
+import express from 'express';
+
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+const port = 3000;
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
