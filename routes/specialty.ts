9af760f2290fe43d72fdbb7c84e0ac8939a2e136
Dair Smith <dair.code@gmail.com>
2021-05-10 16:35:44 -0400

    unnecessary pizza array

--- routes/specialty.ts
+++ routes/specialty.ts
@@ -2,14 +2,8 @@ import express from 'express';
 
 const routes = express.Router();
 
-const specialPizzas = [
-    { name: "Anchovy Lover's", price: 15.00 },
-    { name: "Paleo Pizza", price: 13.00 },
-    { name: "Dessert Pizza", price: 14.00 }
-];
-
 routes.get('/specialty', (req, res) => {
-    const name = req.query.name as string;
+    const name = req.query.name;
     const price = req.query.price;
     res.render("specialty-pizzas", { name, price });
 });
