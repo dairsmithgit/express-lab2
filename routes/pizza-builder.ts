5478f721e772dcdd6dfbb0f2fcdf227aaeef9deb
Dair Smith <dair.code@gmail.com>
2021-05-11 09:27:07 -0400

    pizza builder page and routes

--- routes/pizza-builder.ts
+++ routes/pizza-builder.ts
@@ -2,21 +2,28 @@ import express from 'express';
 
 const routes = express.Router();
 
-const toppings = [
-    "Pepperoni",
-    "Sausage",
-    "Bacon",
-    "Mushrooms",
-    "Green Pepper",
-    "Onion",
-    "Anchovy",
-    "Olive",
-    "Not Pineapple"
-];
-
 routes.get('/pizza-builder', (req, res) => {
-    const topping = toppings;
-    res.render("pizza-builder", { topping });
+    const toppings = [
+        "Pepperoni",
+        "Sausage",
+        "Bacon",
+        "Mushrooms",
+        "Green Pepper",
+        "Onion",
+        "Anchovy",
+        "Olive",
+        "Not Pineapple"
+    ];
+    res.render("pizza-builder", { toppings });
+});
+
+routes.get('/pizza-builder-results', (req, res) => {
+    const size = req.query.size;
+    const toppings: number = parseInt(req.query.toppings as string);
+    const glutenFree: boolean = !!req.query.glutenFree;
+    const specialInstructions = req.query.specialInstructions;
+    let price = 0;
+    
 });
 
 export default routes;
