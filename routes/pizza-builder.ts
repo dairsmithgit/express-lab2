31be13cfcd29a5290b63d54843d51feee571fe66
Dair Smith <dair.code@gmail.com>
2021-05-11 03:06:17 -0400

    pizza builder array and route

---
+++ routes/pizza-builder.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const routes = express.Router();
+
+const toppings = [
+    "Pepperoni",
+    "Sausage",
+    "Bacon",
+    "Mushrooms",
+    "Green Pepper",
+    "Onion",
+    "Anchovy",
+    "Olive",
+    "Not Pineapple"
+];
+
+routes.get('/pizza-builder', (req, res) => {
+    const topping = toppings;
+    res.render("pizza-builder", { topping });
+});
